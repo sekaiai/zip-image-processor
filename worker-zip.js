88ad5aac878320c61198b5ef10e63aef295be4e0
xueyang <313867808@qq.com>
2024-08-23 23:11:57 +0800

    Update worker-zip.js

--- worker-zip.js
+++ worker-zip.js
@@ -4,8 +4,6 @@ import prettyHrtime from 'pretty-hrtime'
 import { Worker, parentPort, workerData } from 'worker_threads'
 import async from 'async'
 
-const imageFormats = ['.png', '.jpg', '.jpeg', '.gif', '.bmp', '.tiff', '.webp', '.svg']
-
 // 处理压缩包
 const processZipFile = async ({
   inputPath,
@@ -25,17 +23,25 @@ const processZipFile = async ({
     const timeStart = process.hrtime()
 
     // 遍历所有文件条目
-    const zipEntries = zip.getEntries()
+    const imageFormatsSet = new Set(['.png', '.jpg', '.jpeg', '.gif', '.bmp', '.tiff', '.webp', '.svg'])
     const options = { maxWidth, outputFormat, quality }
-    await async.eachLimit(zipEntries, sharpThreads, createWorker.bind({ outzip, options }))
+    const zipEntries = zip.getEntries().reduce((acc, entry) => {
+      const ext = path.extname(entry.entryName).toLowerCase()
+      if (imageFormatsSet.has(ext)) {
+        acc.push({ file: entry.getData(), options: { filename: entry.entryName, ...options } })
+      }
+      return acc
+    }, [])
+
+    await async.eachLimit(zipEntries, sharpThreads, createWorker.bind({ outzip }))
 
     // 所有文件添加完成后，写入压缩包
     if (outzip.getEntryCount()) {
       await outzip.writeZipPromise(outputPath)
-    }
 
-    // 移动已完成文件
-    fs.renameSync(inputPath, completedPath)
+      // 移动已完成文件
+      // fs.renameSync(inputPath, completedPath)
+    }
 
     const elapsed = prettyHrtime(process.hrtime(timeStart))
     const arr = [...elapsed.replace(/\s+/, '').split(''), ...Array(20).fill(' ')].slice(0, 9).join('')
@@ -52,16 +58,10 @@ async function compressImage() {
 
 compressImage()
 
-async function createWorker(entry) {
+async function createWorker(workerData) {
   return new Promise(resolve => {
-    if (!imageFormats.includes(path.extname(entry.entryName).toLowerCase())) {
-      return resolve()
-    }
-
     // 创建一个新的 Worker 线程
-    const worker = new Worker('./worker-sharp.js', {
-      workerData: { file: entry.getData(), options: { filename: entry.entryName, ...this.options } }
-    })
+    const worker = new Worker('./worker-sharp.js', { workerData })
 
     // 监听 Worker 线程的消息
     worker.on('message', message => {
